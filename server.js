b09412e8bd1e58dd0fc2e65db5735697a3ec9dec
Eli <elionlink@gmail.com>
2020-07-31 12:37:32 +0300

    310720

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const atlasConnect = require('./DB/atlas');
+const atlasConnect = require('./Data/atlas');
 
 atlasConnect();
 
